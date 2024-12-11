b220cc7634ba7cebf75aea7ecba681ce499bcf35
renardelyon <renard.elyon.r@gmail.com>
2024-12-11 16:26:37 +0700

    Add new task attribute

--- constant/task.go
+++ constant/task.go
@@ -1,11 +1,16 @@
 package constant
 
 type TaskStatus string
+type TaskDaily string
 
 const (
-	INCOMPLETE        = "incomplete"
-	COMPLETE          = "complete"
-	COMPLETE_NO_RECUR = "complete_no_recur"
+	INCOMPLETE = "incomplete"
+	COMPLETE   = "complete"
+)
+
+const (
+	TASK_DAILY    = "task_daily"
+	TASK_ONE_TIME = "task_one_time"
 )
 
 func (ts *TaskStatus) IsValid() (res bool) {
@@ -14,7 +19,21 @@ func (ts *TaskStatus) IsValid() (res bool) {
 	}
 
 	switch *ts {
-	case INCOMPLETE, COMPLETE, COMPLETE_NO_RECUR:
+	case INCOMPLETE, COMPLETE:
+		return true
+
+	}
+
+	return
+}
+
+func (td *TaskDaily) IsValid() (res bool) {
+	if td == nil {
+		return
+	}
+
+	switch *td {
+	case TASK_DAILY, TASK_ONE_TIME:
 		return true
 
 	}
--- domain/tasks/model/task.go
+++ domain/tasks/model/task.go
@@ -7,18 +7,18 @@ import (
 )
 
 type Task struct {
-	TaskId      primitive.ObjectID `bson:"task_id"`
-	Status      string             `bson:"status"`
-	LastUpdated time.Time          `bson:"last_updated"`
+	TaskId      primitive.ObjectID `bson:"task_id" json:"task_id"`
+	Status      string             `bson:"status" json:"status"`
+	LastUpdated time.Time          `bson:"last_updated" json:"last_updated"`
 }
 
 type TaskMaster struct {
-	Id           primitive.ObjectID `bson:"_id,omitempty"`
-	RewardAmount float32            `bson:"reward_amount"`
-	Name         string             `bson:"name"`
-	Description  string             `bson:"description"`
+	Id           primitive.ObjectID `bson:"_id" json:"id"`
+	RewardAmount float32            `bson:"reward_amount" json:"reward_amount"`
+	Name         string             `bson:"name" json:"name"`
+	Description  string             `bson:"description" json:"description"`
+	Type         string             `bson:"type" json:"type"`
 }
-
 type TaskData struct {
 	Task       Task       `bson:"tasks" json:"task"`
 	TaskDetail TaskMaster `bson:"task_detail" json:"task_detail"`
